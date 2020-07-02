f0f80979dd5d1cd47c26f6e9c9ca74c91dfcd51e
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-07-02 14:36:20 +0300

    c

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = (env, argv) => {
@@ -34,6 +35,9 @@ module.exports = (env, argv) => {
             new CleanWebpackPlugin(),
             new HtmlWebpackPlugin({
                 template: "./src/index.html",
+            }),
+            new CopyPlugin({
+                patterns: [{ from: "_redirects", to: "" }],
             }),
         ],
         resolve: {
